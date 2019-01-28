a4bb4fb4298a8d3228867c368e17d8c0824d17a7
zzhou <zzhou@cobo.com>
2019-01-28 15:44:15 +0800

    update go lib

--- go/custody_client.go
+++ go/custody_client.go
@@ -4,19 +4,18 @@ import "fmt"
 import "time"
 import "io"
 import "strings"
+import "crypto/hmac"
 import "sort"
 import "net/url"
-import "crypto/hmac"
 import ioutil "io/ioutil"
 import hex "encoding/hex"
 import sha256 "crypto/sha256"
-import secp256k1 "github.com/decred/dcrd/dcrec/secp256k1"
+import btcec "github.com/btcsuite/btcd/btcec"
 import http "net/http"
 
-const host = "https://api.sandbox.cobo.com"
-const api_key = "x"
-const api_secret = "x"
-
+const HOST = "https://api.sandbox.cobo.com"
+const API_KEY = "x"
+const API_SECRET = "x"
 const SIG_TYPE = "hmac"
 
 func SortParams(params map[string]string) string {
@@ -35,54 +34,58 @@ func SortParams(params map[string]string) string {
 	}
 	return strings.Join(sorted, "&")
 }
-
 func Hash256(s string) string {
 	hash_result := sha256.Sum256([]byte(s))
 	hash_string := string(hash_result[:])
 	return hash_string
 }
-
-func doubleHash256(s string) string {
+func Hash256x2(s string) string {
 	return Hash256(Hash256(s))
 }
-
+func SignHmac(message string) string {
+	h := hmac.New(sha256.New, []byte(API_SECRET))
+	io.WriteString(h, message)
+	return fmt.Sprintf("%x", h.Sum(nil))
+}
 func SignEcc(message string) string {
-	decoded_api_secret, _ := hex.DecodeString(api_secret)
-	privKey, _ := secp256k1.PrivKeyFromBytes(decoded_api_secret)
-	sig, _ := privKey.Sign([]byte(doubleHash256(message)))
+	api_secret, _ := hex.DecodeString(API_SECRET)
+	privKey, _ := btcec.PrivKeyFromBytes(btcec.S256(), api_secret)
+	sig, _ := privKey.Sign([]byte(Hash256x2(message)))
 	return fmt.Sprintf("%x", sig.Serialize())
 }
+func VerifyEcc(message string, signature string) bool {
+	api_key, _ := hex.DecodeString(API_KEY)
+	pubKey, _ := btcec.ParsePubKey(api_key, btcec.S256())
 
-func SignHmac(message string) string {
-    h := hmac.New(sha256.New, []byte(api_secret))
-    io.WriteString(h, message)
-    return fmt.Sprintf("%x", h.Sum(nil))
+	sigBytes, _ := hex.DecodeString(signature)
+	sigObj, _ := btcec.ParseSignature(sigBytes, btcec.S256())
+
+	verified := sigObj.Verify([]byte(Hash256x2(message)), pubKey)
+	return verified
 }
-
 func Request(method string, path string, params map[string]string) string {
 	client := &http.Client{}
-	nonce := fmt.Sprintf("%d", time.Now().Unix() * 1000)
+	nonce := fmt.Sprintf("%d", time.Now().Unix()*1000)
 	sorted := SortParams(params)
 	var req *http.Request
-
 	if method == "POST" {
-		req, _ = http.NewRequest(method, host + path, strings.NewReader(sorted))
+		req, _ = http.NewRequest(method, HOST+path, strings.NewReader(sorted))
 		req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
 	} else {
-		req, _ = http.NewRequest(method, host + path + "?" + sorted, strings.NewReader(""))
+		req, _ = http.NewRequest(method, HOST+path+"?"+sorted, strings.NewReader(""))
 	}
 	content := strings.Join([]string{method, path, nonce, sorted}, "|")
 
-	req.Header.Set("Biz-Api-Key", api_key)
+	req.Header.Set("Biz-Api-Key", API_KEY)
 	req.Header.Set("Biz-Api-Nonce", nonce)
 	if SIG_TYPE == "hmac" {
-        req.Header.Set("Biz-Api-Signature", SignHmac(content))
-    } else if (SIG_TYPE == "ecdsa") {
-        req.Header.Set("Biz-Api-Signature", SignEcc(content))
-    } else {
-        fmt.Printf("Not supported signature type")
-        return ""
-    }
+		req.Header.Set("Biz-Api-Signature", SignHmac(content))
+	} else if SIG_TYPE == "ecdsa" {
+		req.Header.Set("Biz-Api-Signature", SignEcc(content))
+	} else {
+		fmt.Printf("Not supported signature type")
+		return ""
+	}
 
 	resp, _ := client.Do(req)
 
@@ -91,8 +94,12 @@ func Request(method string, path string, params map[string]string) string {
 	body, _ := ioutil.ReadAll(resp.Body)
 	return string(body)
 }
-
 func main() {
-    res := Request("GET", "/v1/custody/org_info/", map[string]string{})
-	fmt.Printf("%v", res)
+	res := Request("GET", "/v1/custody/org_info/", map[string]string{})
+	fmt.Printf("res %v", res)
+	res = Request("GET", "/v1/custody/transaction_history/", map[string]string{
+		"coin": "ETH",
+		"side": "deposit",
+	})
+	fmt.Printf("res %v", res)
 }
