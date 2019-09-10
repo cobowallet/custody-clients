fbd550a8337f6f15650ff8847c8c3ad5d2f7615a
zzhou <zzhou@cobo.com>
2019-09-10 18:05:15 +0800

    change js client

--- js/custody_client.js
+++ js/custody_client.js
@@ -1,18 +1,13 @@
-const crypto = require('crypto');
+//var bitcoin = require('bitcoinjs-lib') // v3.x.x
+//var keyPair = bitcoin.ECPair.fromWIF('L3oe5Wz3wmbxq9tar8nS5bqqCfjMZJNKPXAbbemoHWKXR1SAN8j2')
+//var privateKey = keyPair.privateKey
+/* global require Buffer*/
 const sha256 = require('sha256');
 const bip66 = require('bip66');
 const fetch = require('node-fetch');
 const ec = new require('elliptic').ec('secp256k1')
 
 const ZERO = Buffer.alloc(1, 0)
-
-let host = "https://api.sandbox.cobo.com";
-
-let api_key = "x";
-let api_secret = "x";
-
-let sig_type = 'hmac'
-
 function toDER(x){
     let i = 0
     while (x[i] === 0) ++i
@@ -22,8 +17,15 @@ function toDER(x){
     return x
 }
 
-const sign_ecc = (message, api_secret) =>{
-    let privateKey = Buffer.from(api_secret, 'hex')
+const generate = () => {
+    let key = ec.genKeyPair()
+    return [key.getPublic(true, "hex"), key.getPrivate("hex")]
+}
+
+
+const sign = (message, api_hex) =>{
+    //let message = 'GET|/v1/custody/org_info/|1541560385699|'
+    let privateKey = Buffer.from(api_hex, 'hex')
     let result = ec.sign(Buffer.from(sha256.x2(message), 'hex'), privateKey)
     var r = new Buffer(result.r.toString(16, 64), 'hex')
     var s = new Buffer(result.s.toString(16, 64), 'hex')
@@ -32,66 +34,53 @@ const sign_ecc = (message, api_secret) =>{
     return bip66.encode(r, s).toString('hex');
 };
 
-const sign_hmac = (message, api_secret) => {
-    console.log(message)
-    var x = crypto.createHmac('sha256', api_secret)
-                    .update(message)
-                    .digest('hex');
-    console.log(x);
-    return x
-}
-
-const coboFetch = (method, path, params, api_key, api_secret, host = 'https://api.sandbox.cobo.com') => {
+const coboFetch = (method, path, params, api_key, api_hex, base = 'https://api.sandbox.cobo.com') => {
     let nonce = String(new Date().getTime());
     let sort_params = Object.keys(params).sort().map((k) => {
-        return k + '=' + encodeURIComponent(params[k]).replace(/%20/g,'+');
+        return k + '=' + encodeURIComponent(params[k]);
     }).join('&');
     let content = [method, path, nonce, sort_params].join('|');
-    var signature = '';
-    if (sig_type == 'ecdsa') {
-        signature = sign_ecc(content, api_secret) 
-    } else if (sig_type == 'hmac') {
-        signature = sign_hmac(content, api_secret) 
-    } else {
-        throw "unexpected sig_type " + sig_type;
-    }
-
     let headers = {
         'Biz-Api-Key': api_key,
         'Biz-Api-Nonce': nonce,
-        'Biz-Api-Signature': signature
+        'Biz-Api-Signature': sign(content, api_hex)
     };
-
-    if (method == 'GET') {
-        return fetch(host + path + '?' + sort_params, {
+    if (method == 'GET'){
+        return fetch(base + path + '?' + sort_params, {
             'method': method,
             'headers': headers,
         });
-    } else if (method == 'POST') {
-        headers['Content-Type'] = "application/x-www-form-urlencoded";
-        return fetch(host + path, {
+    }else if (method == 'POST'){
+        return fetch(base + path, {
             'method': method,
             'headers': headers,
-            'body': sort_params
+            'body': JSON.stringify(params)
         });
     }else{
         throw "unexpected method " + method;
     }
 }
 
-coboFetch('POST', '/v1/custody/new_withdraw_request/', 
-        {
-            "coin": "ETH", 
-            "address": "0x8e2782aabdf80fbb69399ce3d9bd5ae69a60462c", 
-            "amount": "100000000000000", 
-            "request_id": "unique_123456",
-            "memo": "hello test"
-        }, 
-        api_key, api_secret, host
-    ).then(res => {
+
+let api_key = 'x';
+let api_hex = 'x';
+
+coboFetch('GET', '/v1/custody/org_info/', {}, api_key, api_hex, 'https://tzeng.dev.cobowallet.cn')
+    .then(res => {
+        console.log(res.status);
         res.json().then((data)=>{
-            console.log(JSON.stringify(data, null, 4));
+            console.log(data);
         })
     }).catch(err => {
         console.log(err)
     });
+coboFetch('GET', '/v1/custody/transaction_history/', {'coin': 'ETH', 'side': 'deposit'}, api_key, api_hex, 'https://tzeng.dev.cobowallet.cn')
+    .then(res => {
+        console.log(res.status);
+        res.json().then((data)=>{
+            console.log(data);
+        })
+    }).catch(err => {
+        console.log(err)
+    });
+
